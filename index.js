7ddb327911dc5cd35338365e3c486fba6914ad8d
nikolaoravec <oravec.nikola@gmail.com>
2022-04-18 15:01:24 +0200

    Add data to .json file and read single data input by :id

--- index.js
+++ index.js
@@ -16,6 +16,8 @@ const pathToFile = path.resolve("./data.json");
 
 const getResorces = () => JSON.parse(fs.readFileSync(pathToFile));
 
+app.use(express.json());
+
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
@@ -23,6 +25,33 @@ app.get("/", (req, res) => {
 app.get("/api/resources", (req, res) => {
   const resources = getResorces();
   res.send(resources);
+});
+
+app.get("/api/resources/:id", (req, res) => {
+  const resources = getResorces();
+  const { id } = req.params;
+
+  const resource = resources.find((resource) => resource.id === id);
+
+  res.send(resource);
+});
+
+app.post("/api/resources", (req, res) => {
+  const resources = getResorces();
+  const resource = req.body;
+
+  resource.createdAt = new Date();
+  resource.status = "inactive";
+  resource.id = Date.now().toString();
+
+  resources.unshift(resource);
+
+  fs.writeFile(pathToFile, JSON.stringify(resources, null, 2), (error) => {
+    if (error) {
+      return res.status(422).send("Cannot store data in the file!");
+    }
+    return res.send("Data has been received");
+  });
 });
 
 app.listen(PORT, () => {
